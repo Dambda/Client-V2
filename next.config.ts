9959a62659668dffa9e77ddc3845ec04f133d053
김예안 <kjan0929@naver.com>
2025-02-03 11:49:21 +0900

    setting: 기본 폰트 적용 및 mixin을 활용한 폰트 스타일 최적화

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   sassOptions: {
-    implementation: "sass-embedded",
+    includePaths: [path.join(__dirname, "styles")],
+    // 특정 코드를 모든 scss 파일 상단에 자동으로 추가
+    additionalData: `@use "@/shared/styles/style.scss" as *;`,
   },
 };
 
