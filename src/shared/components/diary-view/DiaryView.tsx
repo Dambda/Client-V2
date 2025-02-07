36a3503110eb9fe534a6fa5c2a1f48c895a5876a
kojesung <js95112345@gmail.com>
2025-02-07 18:41:01 +0900

    feat : 다이어리 보기 default UI

--- src/shared/components/diary-view/DiaryView.tsx
+++ src/shared/components/diary-view/DiaryView.tsx
@@ -1,3 +1,54 @@
-export default function DiaryView() {
-    return <div>일기 보기 컴포넌트</div>;
+import Image, { StaticImageData } from 'next/image';
+import s from './diary-view.module.scss';
+
+type DiaryViewMode = 'calendar' | 'feature' | 'default';
+
+interface DiaryViewProps {
+    mode: DiaryViewMode;
+    title: string;
+    content: string;
+    coverImg?: string | StaticImageData;
+    date: string;
+}
+
+export default function DiaryView({
+    mode,
+    title,
+    content,
+    coverImg,
+    date,
+}: DiaryViewProps) {
+    switch (mode) {
+        case 'calendar':
+            return <div>달력 페이지</div>;
+        case 'feature':
+            return <div>덧말이나 감정보기... 등 기능</div>;
+        default:
+            return (
+                <div className={s.test}>
+                    <h3 className={s.title}>{title}</h3>
+                    <div className={s.hrContainer}>
+                        <hr className={s.leftHr} />
+                        <span>{date}</span>
+                        <hr className={s.rightHr} />
+                    </div>
+                    <div className={s.contentContainer}>
+                        <span className={s.content}>{content}</span>
+                        {coverImg && (
+                            <Image
+                                className={s.image}
+                                src={coverImg}
+                                alt="커버사진"
+                                width={500}
+                                height={300}
+                            />
+                        )}
+                    </div>
+                    <hr className={s.bottomHr} />
+                    <div className={s.bottomContainer}>
+                        <span>{date}</span>
+                    </div>
+                </div>
+            );
+    }
 }
