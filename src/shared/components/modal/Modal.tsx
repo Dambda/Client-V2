c54e693b0f94a6c57000010c15f15e3b70bc0608
SeHyun <rla6170@naver.com>
2025-02-14 16:54:11 +0900

    feat: Modal 외부 클릭 기능 연결, 중복 컴포넌트 병합

--- src/shared/components/modal/Modal.tsx
+++ src/shared/components/modal/Modal.tsx
@@ -1,45 +1,44 @@
 'use client';
 
+import {
+    ModalProps,
+} from '@/shared/types/useModal.types';
 import S from './modal.module.scss';
-import { useRouter } from 'next/navigation';
-
 /**
- *
+ * 공용 모달 컴포넌트
  * @param children 모달 내부에 들어갈 컨텐츠
+ * @parms modalConfig 모달 설정
  * @returns 닫기 버튼이 존재하지 않는 모달
+ * @example
+  * const { modalConfig, toggleModal } = useModal({ open: true });
+  * <Modal modalConfig={modalConfig}>
+  *    <p>example</p>
+  *    <button onClick={toggleModal}>toggle</button>
+  * </Modal>
  */
-export function Modal({ children }: Readonly<{ children: React.ReactNode }>) {
-    return (
-        <div className={S.modal}>
-            <div className={S.modalContent}>{children}</div>
-        </div>
-    );
-}
-
-/**
- *
- * @param children 모달 내부에 들어갈 컨텐츠
- * @param btnSize 닫기 버튼의 크기(default: 24)
- * @returns 닫기 버튼이 존재하는 모달
- */
-export function CloseModal({
+export function Modal({
     children,
-    btnSize = 24,
+    modalConfig,
 }: Readonly<{
-    children: React.ReactNode;
-    btnSize?: 24 | 32;
+    children?: React.ReactNode;
+    modalConfig: ModalProps;
 }>) {
-    const router = useRouter();
+    if (modalConfig.open === false) return null;
     return (
-        <div className={S.modal}>
-            <div className={S.modalContent}>
-                <button
-                    className={S.closeBtn}
-                    style={{ width: `${btnSize}px`, height: `${btnSize}px` }}
-                    onClick={() => router.back()}
-                >
-                    close
-                </button>
+        <div className={S.modal} onClick={() => modalConfig.outerTouchClose && modalConfig.handleClose()}>
+            <div className={S.modalContent} onClick={(e) => e.stopPropagation()}>
+                {modalConfig.outerTouchClose && (
+                    <button
+                        className={S.closeBtn}
+                        style={{
+                            width: `${modalConfig.btnSize}px`,
+                            height: `${modalConfig.btnSize}px`,
+                        }}
+                        onClick={() => modalConfig.handleClose()}
+                    >
+                        close
+                    </button>
+                )}
                 {children}
             </div>
         </div>
