f7eecb2161b3d987a44330904300db33f892bc96
김예안 <kjan0929@naver.com>
2025-02-14 14:47:41 +0900

    setting: svgr 세팅 및 예시 코드 추가

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,23 @@ import type { NextConfig } from 'next';
 import path from 'path';
 
 const nextConfig: NextConfig = {
+    webpack: (config) => {
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: ['@svgr/webpack'],
+        });
+        return config;
+    },
+    experimental: {
+        turbo: {
+            rules: {
+                '*.svg': {
+                    loaders: ['@svgr/webpack'],
+                    as: '*.js',
+                },
+            },
+        },
+    },
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
         // 특정 코드를 모든 scss 파일 상단에 자동으로 추가
---
+++ src/app/test/svgr/page.tsx
@@ -0,0 +1,10 @@
+import JoyEmblem from './joy-emblem.svg';
+
+export default function TestPage() {
+    return (
+        <div>
+            <h2>TestPage</h2>
+            <JoyEmblem width={120} height={120} fill="skyblue" />
+        </div>
+    );
+}
