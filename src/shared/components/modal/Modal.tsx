3123ed23b8c342833590caa076c4e393c916da73
SeHyun <rla6170@naver.com>
2025-02-14 10:53:59 +0900

    fix: sonarCloud code smell(:readOnly) 수정, css duplicate(:display) 수정

--- src/shared/components/modal/Modal.tsx
+++ src/shared/components/modal/Modal.tsx
@@ -8,7 +8,7 @@ import { useRouter } from 'next/navigation';
  * @param children 모달 내부에 들어갈 컨텐츠
  * @returns 닫기 버튼이 존재하지 않는 모달
  */
-export function Modal({ children }: { children: React.ReactNode }) {
+export function Modal({ children }: Readonly<{ children: React.ReactNode }>) {
     return (
         <div className={S.modal}>
             <div className={S.modalContent}>{children}</div>
@@ -25,10 +25,10 @@ export function Modal({ children }: { children: React.ReactNode }) {
 export function CloseModal({
     children,
     btnSize = 24,
-}: {
+}: Readonly<{
     children: React.ReactNode;
     btnSize?: 24 | 32;
-}) {
+}>) {
     const router = useRouter();
     return (
         <div className={S.modal}>
